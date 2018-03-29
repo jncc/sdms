83804141a36bd7727b0356c22e4f27fbc95dbfad
hutchisonjtw <james.hutchison@jncc.gov.uk>
2018-03-29 10:26:14 +0100

    Replaced comments with Roxygen documentation.

--- R/FunctionOccurrence.R
+++ R/FunctionOccurrence.R
@@ -1,10 +1,12 @@
-## Biodiversity Module
-## Function to prepare create occurrence data from presence points by adding random jitter
-## Add random amount to eastings/nrothings to place coordinates
-## randomly within resolution grid cell
-## If capture resolution is higher than covariable resolution, 
-## just take grid cell coords, no gain from using random point locality
-## Originally based on script from BiodiversityMod_v8.R
+#' Place occurence points at random within low-resolution grid cell
+#'
+#' This function is used to randomise the location of a presence point where the species occurrence data is low resolution. For example, if your species occurrence data is at 1km resolution, but your environmental predictor variables are at 200m resolution, this enables you to randomise where within the 1km cell your species record is placed. The function can handle multiple resolutions within a single dataframe, as long as these are labelled with a precision column.
+#'
+#' @param presframe Data frame of presence points. This should include columns titled "easting" and "northing", which should be x and y coordinates in metres (of the centre of the grid cell for gridded data), as well as a column giving the resolution (see \code{prescisionCol} below).
+#' @param precisionCol The column indicating the resolution of the presence point data. This should be given as a grid cell size in metres e.g. for a 1km grid this should be 1000.
+#' @return A copy of the input dataframe \code{presframe}, but with new "easting" and "northing" values that place each point randomly within it's grid cell.
+#' @export
+
 
 randomOcc <- function(presframe, #presence data frame
                       precisionCol = "precision") { #which column defines capture resolution
@@ -16,7 +18,7 @@ randomOcc <- function(presframe, #presence data frame
         # if(unit == "km") {movemax <- 500 * j}
         # if(unit == "m") {movemax <- 0.5 * j}
         movemax <- 0.5 * j #Half of capture resolution grid cell size
-        df$easting <- df$easting + round(runif(length(df$easting), -movemax, movemax)) #Move east/west by up to half capture grid cell size 
+        df$easting <- df$easting + round(runif(length(df$easting), -movemax, movemax)) #Move east/west by up to half capture grid cell size
         df$northing <- df$northing + round(runif(length(df$northing), -movemax, movemax)) #Move north/south by up to half capture grid cell size
         occurrence <- rbind(occurrence, df)
       } else {
