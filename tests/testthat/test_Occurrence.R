a7209a4973d5bb0cdc513323cf4a3229cd8415d1
BeckyTrippier <Becky.Trippier@jncc.gov.uk>
2018-05-22 16:20:53 +0100

    Test for occurrence function.

---
+++ tests/testthat/test_Occurrence.R
@@ -0,0 +1,28 @@
+## Tests to check the functionality of the Package function - Occurrence
+#  Becky Trippier 22/05/2018
+#### ----------------------
+
+load("D:/Github/sdms/R/sysdata.rda")
+
+context("Occurrence tests - general")
+
+occ <- bngprep(speciesdf = sp_gatewaydata, bngCol = "gridReference", datafrom =
+                      "NBNgateway", minyear = 2007, maxyear = 2014, covarRes = 300)
+occurrence <- raster::as.data.frame(occ)
+occurrence <- randomOcc(presframe = occurrence, precisionCol = "precision", covarResm = 300)
+
+test_that("function returns dataframe with easting and northing", {
+
+  expect_is(occurrence, "data.frame")
+
+})
+
+test_that("function returns dataframe with different easting and northing", {
+
+  before <- sample_n(occ, 10)
+  after <- subset(occurrence, NBNObservationID %in% before$NBNObservationID)
+  before <- before[order(before$NBNObservationID),]
+  after <- after[order(after$NBNObservationID),]
+  expect_false(unique(before$easting == after$easting))
+  expect_false(unique(before$northing == after$nothing))
+})
