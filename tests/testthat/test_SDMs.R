e0d3b948ef5758117ab1b7bce668c0c96ec2787b
BeckyTrippier <Becky.Trippier@jncc.gov.uk>
2018-05-24 12:38:26 +0100

    Removed vars from internal data and updated tests for SDMs function

--- tests/testthat/test_SDMs.R
+++ tests/testthat/test_SDMs.R
@@ -9,7 +9,9 @@ gatspdat <- bngprep(speciesdf = sp_gatewaydata, bngCol = "gridReference", datafr
                       "NBNgateway", minyear = 2007, maxyear = 2014, covarRes = 300)
 sp::coordinates(gatspdat)<- ~ easting + northing
 
-final_out <- SDMs(occ = gatspdat, max_tries = 1, models = "BioClim", lab = "test", bckg = gatbackground, rndm_occ = TRUE)
+#raster stack of predictor variables - vars
+load(file = "D:/Becky docs/SDMs/varsLCM2015")
+
 
 
 test_that("model input error when these are incorrectly defined", {
@@ -18,15 +20,27 @@ test_that("model input error when these are incorrectly defined", {
 })
 
 test_that("function places random occurrences when defined as TRUE", {
+  expect_condition(SDMs(occ = gatspdat, max_tries = 1, models = "BioClim", lab = "test", bckg = gatbackground, rndm_occ = TRUE), "Random occurrences placed")
 
 })
 
-test_that("test output files produced", {
+library(glmulti)
+SDMs(occ = gatspdat, max_tries = 1, lab = "test", bckg = gatbackground, rndm_occ = TRUE)
 
+test_that("test output files produced", {
+expect_true(exists("all_evals") == TRUE)
+expect_true(exists("all_models") == TRUE)
+expect_true(exists("all_predicts") == TRUE)
 })
 
 test_that("test all models run on start up when selected", {
+  evals <- as.data.frame(unlist(all_evals[2]))
+  colnames(evals)[1] <- "score"
+  expect_true(all(evals$score != "NA") == TRUE)
 
 })
+
+
+
 
 
