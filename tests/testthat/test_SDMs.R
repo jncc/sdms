2dade28e59361cfb418a5268c91ed08ca0cc4ab1
BeckyTrippier <Becky.Trippier@jncc.gov.uk>
2018-05-24 12:05:18 +0100

    added initial tests to ensure defined models are input and if max_tries exceeds 100 the user is prompted whether to continue

--- tests/testthat/test_SDMs.R
+++ tests/testthat/test_SDMs.R
@@ -2,41 +2,22 @@
 #  Becky Trippier 22/05/2018
 #### ----------------------
 
+#get occurrence data for tests
 load("D:/Github/sdms/R/sysdata.rda")
 
-context("SDM tests - general")
+gatspdat <- bngprep(speciesdf = sp_gatewaydata, bngCol = "gridReference", datafrom =
+                      "NBNgateway", minyear = 2007, maxyear = 2014, covarRes = 300)
+sp::coordinates(gatspdat)<- ~ easting + northing
+
+final_out <- SDMs(occ = gatspdat, max_tries = 1, models = "BioClim", lab = "test", bckg = gatbackground, rndm_occ = TRUE)
 
 
-#occ <- bngprep(speciesdf = sp_gatewaydata, bngCol = "gridReference", datafrom =
-#                 "NBNgateway", minyear = 2007, maxyear = 2014, covarRes = 300)
-#sp::coordinates(ev_spdat)<- ~ easting + northing
-
-# get background mask
-#load("Z:/Prog100-TerrestrialEvidence/Innov- Natural capital models/Data/BGmasks/amphibian")
-
-#if(exists("mask1km")){
-#  background <- mask1km
-#  rm(mask1km)
-#} else {
-#  r <- vars[[1]]
-#  r[!is.na(r[])] <- 1
-#  background <- r
-#  rm(r)
-#}
-
-#raster stack of predictor variables - vars
-#load(file = "D:/Becky docs/SDMs/varsLCM2015")
-
-##run SDM function
-#final_out <- SDMs(occ = ev_spdat, max_tries = 2, lab = "ev", rndm_occ = TRUE)
-
-
-
-test_that("function places random occurrences when defined as TRUE", {
+test_that("model input error when these are incorrectly defined", {
+  expect_error(SDMs(occ = gatspdat, max_tries = 1, models = "Bioclim", lab = "test", bckg = gatbackground, rndm_occ = TRUE), "Model specification contains an unexpected value, please check model names input. Operation terminated.")
 
 })
 
-test_that("test all models run on start up when selected", {
+test_that("function places random occurrences when defined as TRUE", {
 
 })
 
@@ -44,6 +25,8 @@ test_that("test output files produced", {
 
 })
 
-test_that("max tries can't exceed 100", {
+test_that("test all models run on start up when selected", {
 
 })
+
+
