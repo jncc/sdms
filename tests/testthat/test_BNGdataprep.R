5be28a6313fba473c0223683683d72702d0f21ae
BeckyTrippier <Becky.Trippier@jncc.gov.uk>
2018-05-31 11:16:00 +0100

    Update tests to use data from Data folder

--- tests/testthat/test_BNGdataprep.R
+++ tests/testthat/test_BNGdataprep.R
@@ -2,69 +2,10 @@
 #  Becky Trippier 21/05/2018
 #### ----------------------
 
-load("D:/Github/sdms/R/sysdata.rda")
-
-context("BNGdataprep - general")
-
-
-gatspdat <- bngprep(speciesdf = sp_gatewaydata, bngCol = "gridReference", datafrom =
-                        "NBNgateway", minyear = 2007, maxyear = 2014, covarRes = 300)
-
-test_that("function returns dataframe of presence only records", {
-
-  expect_is(gatspdat, "data.frame")
-  expect_false(unique(gatspdat$zeroAbundance))
-
-})
-
-test_that("maxyear smaller than minyear warning", {
-
-  expect_error(bngprep(speciesdf = sp_gatewaydata, bngCol = "gridReference", datafrom = "NBNgateway", minyear = 2007, maxyear = 2000, covarRes = 300),"minimum year limit is greater than maximum year")
-})
-
-test_that("where no datafrom specified, function will still run", {
-  sp_gatewaydata$pres <- 1000
-  sp_gatewaydata$year <- as.numeric(format(as.Date(sp_gatewaydata$startDate,
-                                              format = "%d/%m/%Y"), "%Y"))
-  expect_warning(spdat <- bngprep(speciesdf = sp_gatewaydata,  bngCol = "gridReference", mindata = 5000, minyear = 2007, maxyear = 2014, covarRes = 300), "datafrom not specified as NBNatlas or NBNgateway." )
-  expect_is(spdat, "data.frame")
-})
-#-----------------------------------------#
-gatspdat <- bngprep(speciesdf = sp_gatewaydata, bngCol = "gridReference", datafrom =
-                      "NBNgateway", minyear = 2007, maxyear = 2014, covarRes = 300)
-
-context("BNGdataprep - Gateway Example")
-
-test_that("function returns dataframe of presence only records", {
-
-  expect_is(gatspdat, "data.frame")
-  expect_false(unique(gatspdat$zeroAbundance))
-
-})
-
-test_that("Subsetting dates returns within the correct range of years", {
-
-  expect_true(min(gatspdat$year) == 2007)
-  expect_true(max(gatspdat$year) == 2014)
-  expect_false(unique(is.na(gatspdat$year)))
-
-  })
-
-test_that("easting and northing fields added and complete", {
-
-  x<- gatspdat$easting
-  y<- gatspdat$northing
-  expect_true(is.numeric(x))
-  expect_true(is.numeric(y))
-  expect_false(unique(is.na(x)))
-  expect_false(unique(is.na(y)))
-
-})
-
-#-----------------------------------------#
 context("BNGdataprep - Atlas Example")
-ng_data<- read.csv(file="Inputs/Notonecta_glauca.csv", header=TRUE, sep=",", check.names = FALSE, strip.white = TRUE)
-ngspdat <- bngprep(speciesdf = ng_data,  bngCol = "OSGR", datafrom = "NBNatlas", mindata = 5000, minyear = 2007, maxyear = 2014, covarRes = 300)
+ng_data<- read.csv(file="D:/Github/sdms/Data/Inputs/Notonecta_glauca.csv", header=TRUE, sep=",", check.names = FALSE, strip.white = TRUE)
+
+ngspdat <- bngprep(speciesdf = ng_data,  bngCol = "OSGR", datafrom = "NBNatlas", mindata = 5000, minyear = 2007, maxyear = 2012, covarRes = 300)
 
 test_that("function returns dataframe of presence only records", {
   expect_is(ngspdat, "data.frame")
@@ -74,7 +15,7 @@ test_that("function returns dataframe of presence only records", {
 
 test_that("Subsetting dates returns within the correct range of years", {
   expect_true(min(ngspdat$year) == 2007)
-  expect_true(max(ngspdat$year) == 2014)
+  expect_true(max(ngspdat$year) == 2012)
   expect_false(unique(is.na(ngspdat$year)))
 
 })
@@ -100,4 +41,17 @@ expect_is(out, "data.frame")
 
 test_that("Duplicated field names are handled", {
   expect_true(unique(names(ng_data)[44:45] != names(ngspdat)[44:45]))
+})
+
+
+test_that("maxyear smaller than minyear warning", {
+  expect_error(bngprep(speciesdf = ng_data,  bngCol = "OSGR", datafrom = "NBNatlas", minyear = 2007, maxyear = 2000, covarRes = 300),"minimum year limit is greater than maximum year")
+})
+
+
+test_that("where no datafrom specified, function will still run", {
+  ng_data$precision <- 1000
+  names(ng_data)[names(ng_data) == "Year"] <- "year"
+  expect_warning(spdat <- bngprep(speciesdf = ng_data,  bngCol = "OSGR", mindata = 5000, minyear = 2007, maxyear = 2014, covarRes = 300), "datafrom not specified as NBNatlas or NBNgateway." )
+  expect_is(spdat, "data.frame")
 })
