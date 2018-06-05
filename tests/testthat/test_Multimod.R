cf5b7bfc2c833f9b0754f342e038c7f5128360b3
BeckyTrippier <Becky.Trippier@jncc.gov.uk>
2018-06-05 15:25:17 +0100

    Updated tests for the Multimod function to test species list reporting and create temporary folders for testing purposes which are removed after running.

--- tests/testthat/test_Multimod.R
+++ tests/testthat/test_Multimod.R
@@ -1,34 +1,67 @@
 ## Tests to check the functionality of the Package function - Multimod
-#  Becky Trippier 22/05/2018
+#  Becky Trippier 05/06/2018
 #### ----------------------
 
-raster::rasterOptions(tmpdir = "./Rtmpdir")
-
-
-#get species list
-sp_list <- c("Notonecta_glauca", "Sigara_dorsalis")
-
-## raster stack of predictor variables - vars
-data(vars)
-
-path <- rprojroot::find_testthat_root_file()
-out_flder = paste(path, "/Outputs/", sep="")
-dat_flder = paste(path, "/Inputs/", sep="")
-bkgd_flder = paste(path, "/BGmasks/", sep="")
-
-
-
-out <- Multi_mod(sp_list = sp_list, out_flder = out_flder, dat_flder = dat_flder, bkgd_flder = bkgd_flder, vars, max_tries = 1, datafrom = "NBNatlas", minyear = 0, maxyear = 0, mindata = 5000, covarRes = 100, models = "BioClim", prop_test_data = 0.25, bngCol = "OSGR", mult_prssr = FALSE, rndm_occ = TRUE)
 
 context("Multimod tests - general")
 
-test_that("test if lists containing duplicates are handled", {
-  sp_list <- c("sp_gatewaydata", "sp_atlasdata", "sp_atlasdata")
+### set working directory for tests
+start <- getwd()
+setwd(tempdir())
 
+## create test folders with data
+dir.create("Outputs")
+dir.create("Inputs")
+dir.create("BGmasks")
+data("ng_data")
+ng_data$taxonGroup <- "Hemiptera"
+data("sd_data")
+sd_data$taxonGroup <- "Hemiptera"
+data("background")
+utils::write.csv(ng_data, file = "./Inputs/Notonecta_glauca.csv")
+utils::write.csv(ng_data, file = "./Inputs/Sigara_dorsalis.csv")
+save(background, file = "./BGmasks/Hemiptera")
+
+#### get test data
+data(vars)
+
+#### testing
+
+test_that("test if lists containing duplicates are handled", {
+  sp_list <- c("Notonecta_glauca", "Sigara_dorsalis", "Notonecta_glauca")
   expect_message(if (any(duplicated(sp_list)) == TRUE) {
     sp_list <- unique(sp_list)
     message("Duplicate species removed.")
   },"Duplicate species removed.")
+
 })
 
-unlink("./Rtmpdir/*")
+test_that("test if sp_lists contain names not found in input folder", {
+  sp_list <- c("fakeosaurus")
+  expect_error(Multi_mod(sp_list = sp_list, vars, out_flder = "Outputs/",dat_flder = "Inputs/", bkgd_flder = "BGmasks/", max_tries = 1, datafrom = "NBNatlas", covarRes = 100, models = "BioClim", prop_test_data = 0.25, bngCol = "OSGR", mult_prssr = FALSE, rndm_occ = TRUE),"No species found. Check input data folder and file formats.")
+
+})
+
+test_that("test if iterate through list and species complete messages generated", {
+  sp_list <- c("Notonecta_glauca", "Sigara_dorsalis")
+
+   out <- Multi_mod(sp_list = sp_list, vars, out_flder = "Outputs/",dat_flder = "Inputs/", bkgd_flder = "BGmasks/", max_tries = 1, datafrom = "NBNatlas", covarRes = 100, models = "BioClim", prop_test_data = 0.25, bngCol = "OSGR", mult_prssr = FALSE, rndm_occ = TRUE)
+
+   expect_true(file.exists("./Outputs/Notonecta_glauca1.csv") == TRUE)
+   expect_true(file.exists("./Outputs/Sigara_dorsalis1.tif") == TRUE)
+})
+
+test_that("test if already done list is updated", {
+  sp_list <- c("Notonecta_glauca", "Sigara_dorsalis")
+
+   expect_error(Multi_mod(sp_list = sp_list, vars, out_flder = "Outputs/",dat_flder = "Inputs/", bkgd_flder = "BGmasks/", max_tries = 1, datafrom = "NBNatlas", covarRes = 100, models = "BioClim", prop_test_data = 0.25, bngCol = "OSGR", mult_prssr = FALSE, rndm_occ = TRUE),"No more species to process. Modelling terminated.")
+
+
+})
+
+
+
+# remove temporary files and return to working directory
+unlink("Outputs", recursive=TRUE)
+setwd(start)
+unlink(tempdir(), recursive=TRUE)
